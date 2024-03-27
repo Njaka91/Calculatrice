488ae7d0823674f70088b4f9ca44bce9a1ddf5ba
Njaka91 <andriaravelo91@gmail.com>
2024-03-27 10:08:29 +0300

    ajout de commentaire dans le test unitaire

--- test/calculatrice/tp/test/unitaire/test/CalculTest.java
+++ test/calculatrice/tp/test/unitaire/test/CalculTest.java
@@ -26,6 +26,7 @@ public class CalculTest {
         Calcul numbersPositiv = new Calcul(1, 2);
         Calcul numbersNegativ = new Calcul(-4, 2);
         Calcul numbersZero = new Calcul(-4, 0);
+        //verification des scenaria de test
         assertEquals(3, numbersPositiv.addition(), 0.001);
         assertEquals(-2, numbersNegativ.addition(), 0.001);
         assertEquals(-4, numbersZero.addition(), 0.001);
@@ -34,9 +35,11 @@ public class CalculTest {
     @Test
     public void testSubstraction () 
     {
+        //On prépare le scénario de test
         Calcul numbersPositiv = new Calcul(1, 2);
         Calcul numbersNegativ = new Calcul(-4, 2);
         Calcul numbersZero = new Calcul(4, 0);
+        //verification des scenaria de test
         assertEquals(-1, numbersPositiv.substraction(), 0.001);
         assertEquals(-6, numbersNegativ.substraction(), 0.001);
         assertEquals(4, numbersZero.substraction(), 0.001);
@@ -45,9 +48,11 @@ public class CalculTest {
     @Test
     public void testMultiplication () 
     {
+        //On prépare le scénario de test
         Calcul numbersPositiv = new Calcul(1, 2);
         Calcul numbersNegativ = new Calcul(-4, 2);
         Calcul numbersZero = new Calcul(4, 0);
+        //verification des scenaria de test
         assertEquals(2, numbersPositiv.multiplication(), 0.001);
         assertEquals(-8, numbersNegativ.multiplication(), 0.001);
         assertEquals(0, numbersZero.multiplication(), 0.001);
@@ -56,10 +61,12 @@ public class CalculTest {
     @Test
     public void testDivisioin () 
     {
+        //On prépare le scénario de test
        Calcul numbersPositiv = new Calcul(1, 2);
         Calcul numbersNegativ = new Calcul(-4, 2);
         Calcul numbersZero = new Calcul(0, 4);
         Calcul numbersInfiny = new Calcul(4, 0);
+        //verification des scenaria de test
         assertEquals(0.5, numbersPositiv.division(), 0.001);
         assertEquals(-2, numbersNegativ.division(), 0.001);
         assertEquals(0, numbersZero.division(), 0.001);
